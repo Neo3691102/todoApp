43abc3a357a31f17c6035cf4b78d9fc4c30c4ca1
Neo3691102 <nestor8772@gmail.com>
2025-03-22 23:05:38 -0600

    Se agrega la estructura básica del footer

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const dialogo = document.getElementById("confirmar-cerrar-dialogo");
-dialogo.showModal();
 
 const cancelBtn = document.getElementById("cancel-btn");
 cancelBtn.addEventListener("click", () => {
-    dialogo.close();
+  dialogo.close();
 });
